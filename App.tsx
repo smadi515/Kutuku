d430f5ea88903ead62d3667f31284194afe2accd
smadi515 <senan.smadi515@gmail.com>
2025-07-14 17:44:49 +0300

    refactor: remove Google Sign-In integration and update dependencies
    - Removed Google Sign-In configuration and related imports from App.tsx.
    - Deleted ImagePickerComponent as it was no longer needed.
    - Updated package.json and package-lock.json to remove Google Sign-In and image picker dependencies, and upgraded several packages to their latest versions.

--- App.tsx
+++ App.tsx
@@ -40,17 +40,11 @@ import AddressScreen from './screens/AddressScreen';
 import OTPScreen from './screens/OTPScreen';
 import EditAddressScreen from './screens/EditAddressScreen';
 import Home from './screens/HomeScreen';
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import Toast from 'react-native-toast-message';
 import messaging from '@react-native-firebase/messaging';
 import {CurrencyProvider} from './contexts/CurrencyContext'; // adjust path if needed
 
-GoogleSignin.configure({
-  webClientId:
-    '1088217967971-6bta2dea5scfouec3ng7qpjfkkedmbhq.apps.googleusercontent.com',
-  // from Google Console (Web type)
-  offlineAccess: true,
-});
+
 // Prevent RTL layout
 I18nManager.allowRTL(false);
 I18nManager.forceRTL(false);
